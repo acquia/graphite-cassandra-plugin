79b3a20e53af8ca557f9b11c943bcaa23e88a3ac
Aaron Morton <aaron@thelastpickle.com>
2014-01-15 10:16:07 +1300

    Remove unnecessary imports
    after the change in #2 to remove the metaclass, it's no longer
    necessary to import the CassandraDatabase from graphite-web

--- graphite_cassandra_plugin/graphite_cassandra_plugin/__init__.py
+++ graphite_cassandra_plugin/graphite_cassandra_plugin/__init__.py
@@ -1,18 +1,12 @@
-from graphite.database import GraphiteDatabase
 
-try:
-  import graphite_cassandra_plugin
-except ImportError:
-  pass
-else:
-  class GraphiteCassandraPlugin(GraphiteDatabase):
-    plugin_name = 'cassandra'
+class GraphiteCassandraPlugin(object):
 
-    def __init__(self, settings):
-      self.keyspace = settings.CASSANDRA_KEYSPACE
-      self.servers = settings.CASSANDRA_SERVERS
-      
-      self.finder = graphite_cassandra_plugin.CassandraFinder(self.keyspace, self.servers)
+  def __init__(self, settings):
+    self.keyspace = settings.CASSANDRA_KEYSPACE
+    self.servers = settings.CASSANDRA_SERVERS
+    
+    self.finder = graphite_cassandra_plugin.CassandraFinder(self.keyspace, 
+      self.servers)
 
-    def finder(self):
-      return self.finder
+  def finder(self):
+    return self.finder
